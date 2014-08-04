ba1c7ec904b0017925fa6e79b3ae64bd6eb4ec7f
wilbown <wilbown@gmail.com>
2014-08-03 22:58:24 -0600

    - copyrights

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2011-2013 The PPCoin developers
+// Copyright (c) 2014-2014 The Noocoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
